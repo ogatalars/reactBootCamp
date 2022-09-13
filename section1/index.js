41dd09e3bdb45a342738f3c383e1c5af4ba0be77
ogatalars <ogataogara@gmail.com>
2022-09-12 22:45:30 -0300

    Diferences between class components and function comp

--- section1/index.js
+++ section1/index.js
@@ -1,13 +1,30 @@
-class Hello extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello there!</h1>
-				<h1>Hello there!</h1>
-				<h1>Hello there!</h1>
-			</div>
-		);
-	}
+// class Hello extends React.Component {
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 			</div>
+// 		);
+// 	}
+// }
+// this first way is the classic way - classes
+
+// here is tehe second way - function way - hooks
+
+function Hello() {
+  return (
+    <div>
+      <h1> Hello There!</h1>
+      <h1> Hello There! </h1>
+    </div>
+  );
 }
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+// So whats the difference?
+// - Both can accept props and render content
+// - Historically, function components coundn`t use important features like - state and lifecycle methods
+// - With the introduction of Hooks, we can now write full-feature function components
+
+ReactDOM.render(<Hello />, document.getElementById("root"));
---
+++ section2/basicRulesJSX.md
@@ -0,0 +1,7 @@
+- JSX isn't legal JavaScript
+  it has to be transpiled to JS
+  you can do this with BABEL
+
+RULES -> JSX is more strict than HTML - elements must either:
+Have an explicit closing tag: <b> ... </b>
+Be explicitly self-closed: <input name = "msg"/> cannot leav off that / or wll get syntax error
